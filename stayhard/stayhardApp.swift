42ecd9cbee3ea8b150463ed50ea23eee44a7387c
michael-bautista <mrbautistaios@gmail.com>
2024-03-16 14:56:36 -0700

    Initial Commit

---
+++ stayhard/stayhardApp.swift
@@ -0,0 +1,17 @@
+//
+//  stayhardApp.swift
+//  stayhard
+//
+//  Created by Michael Bautista on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct stayhardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
